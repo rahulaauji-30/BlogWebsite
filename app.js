d8857d2b65999541d2e2e39e72bee81590b84985
rahulaauji-30 <rahulaauji71@gmail.com>
2023-12-15 10:15:40 +0530

    1.0.1

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port  = 3000;
+let posts = [];
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs",posts);
+})
+
+app.get("/addposts",(req,res)=>{
+    res.render("addPosts.ejs");
+})
+app.post("/addposts",(req,res)=>{
+    posts.push({title:req.body.title,description:req.body.description});
+    res.redirect("/");
+    console.log(posts);
+})
+app.get("/edit",(req,res)=>{
+    res.render("edit.ejs")
+})
+app.patch("/edit",(req,res)=>{
+    
+})
+
+app.delete("/delete",(req,res)=>{
+    posts.forEach((post)=>{
+        if(post ==req.body.title){
+
+        }
+    })
+})
+app.listen(port,()=>{
+    console.log(`Server running on ${port}`);
+})
